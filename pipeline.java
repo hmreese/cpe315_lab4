6cc967210886f0ec746e74cb55c281f92000728b
hmreese <49246395+hmreese@users.noreply.github.com>
2021-02-17 13:11:26 -0800

    Update and rename pipline.java to pipeline.java

--- pipeline.java
+++ pipeline.java
@@ -1,8 +1,8 @@
-public class pipline {
+public class pipeline {
     String pipe[];
     int cycles;
 
-    public pipline(){
+    public pipeline(){
         this.pipe = new String[4];
         this.cycles = 0;
         for(int i = 0; i < 4; i++){
