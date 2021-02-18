619cab646dc17ee4ff4015d7a8575f2cbd2d26bb
hmreese <49246395+hmreese@users.noreply.github.com>
2021-02-17 16:34:03 -0800

    Update pipeline.java

--- pipeline.java
+++ pipeline.java
@@ -2,11 +2,19 @@ public class pipeline {
     String pipe[];
     int cycles;
     int instructions;
+    int pipePC;
+    int delay;
+    boolean isEmpty;
 
-    public pipeline(){
+    public pipeline()
+    {
         this.pipe = new String[4];
         this.cycles = 0;
         this.instructions = 0;
+        this.pipePC = 0;
+        this.delay = 0;
+        this.isEmpty = false;
+
         for(int i = 0; i < 4; i++){
             this.pipe[i] = "empty";
         }
